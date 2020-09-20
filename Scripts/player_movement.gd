cd4eba518fc0f2548ab671c1750a9e1565ed33ed
Christian Bortolotti <cbortolo@purdue.edu>
2020-09-20 19:40:36 -0400

    Basic Gravity
    - Created temporary collision system for movement testing purposes.
    - Player is now influenced by gravity, which has been created as an export var and is therefore adjustable in the inspector

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -1,9 +1,10 @@
-extends Node
+extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Member variables here
+export var playerGravity = 9.81
+var playerVelocity = Vector2()
+var playerDistance
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,6 +12,11 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Called every phys frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	
+	playerVelocity.y += delta * playerGravity 
+	
+	# distance = velocity * time (right?)
+	playerDistance = playerVelocity * delta
+	move_and_collide(playerDistance)
