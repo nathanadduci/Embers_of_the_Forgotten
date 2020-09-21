58eb2a27555960981ae078fb6914f39b0c0bb0c2
Christian Bortolotti <cbortolo@purdue.edu>
2020-09-21 14:30:36 -0400

    LR Movement Polish
    - Player velocity in the y direction will remain the force of gravity while the player is on the floor, and will only increase from that when airborne.
    - Made slight adjustment to float denominator to make things less
    - Holding down will now fast fall (increase the falling speed while the player is in the air and hasn't reached terminal velocity)

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -4,7 +4,8 @@ extends KinematicBody2D
 # Member variables here
 export var playerGravity = 9.81
 export var playerSpeed = 400
-export var terminalVelocity = 850
+export var terminalVelocity = 1500
+export var floatDenominator = 1.3
 var playerVelocity = Vector2()
 var playerDistance
 
@@ -18,10 +19,15 @@ func _ready():
 func _physics_process(delta):
 	
 	# obtain new y velocity
-	if playerVelocity.y < terminalVelocity:
-		playerVelocity.y += delta * playerGravity 
-	else: 
-		playerVelocity.y = terminalVelocity
+	if is_on_floor():
+		playerVelocity.y = playerGravity
+	else:
+		if playerVelocity.y < terminalVelocity:
+			playerVelocity.y += delta * playerGravity 
+			if Input.is_action_pressed("ui_down"):
+				playerVelocity.y += 50
+		else: 
+			playerVelocity.y = terminalVelocity
 	print(playerVelocity.y)
 	
 	#obtain new x velocity
@@ -40,6 +46,6 @@ func _inputSequence():
 		playerVelocity.x = -playerSpeed
 	else:
 		if playerVelocity.x > 1 || playerVelocity.x < -1:
-			playerVelocity.x = playerVelocity.x / 1.1
+			playerVelocity.x = playerVelocity.x / floatDenominator
 		else:
 			playerVelocity.x = 0
