5e619f54c1f1d562258d1f5b62492fc25d45bf3e
Christian Bortolotti <cbortolo@purdue.edu>
2020-09-21 14:14:04 -0400

    LR Improvements
    - Added terminal velocity to avoid exaggerated freefalls
    - Slight float on LR movement to simulate a more natural movement (time taken to "come to a stop")

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -4,6 +4,7 @@ extends KinematicBody2D
 # Member variables here
 export var playerGravity = 9.81
 export var playerSpeed = 400
+export var terminalVelocity = 850
 var playerVelocity = Vector2()
 var playerDistance
 
@@ -17,7 +18,11 @@ func _ready():
 func _physics_process(delta):
 	
 	# obtain new y velocity
-	playerVelocity.y += delta * playerGravity 
+	if playerVelocity.y < terminalVelocity:
+		playerVelocity.y += delta * playerGravity 
+	else: 
+		playerVelocity.y = terminalVelocity
+	print(playerVelocity.y)
 	
 	#obtain new x velocity
 	_inputSequence()
@@ -26,13 +31,15 @@ func _physics_process(delta):
 	# playerDistance = playerVelocity * delta
 	move_and_slide(playerVelocity, Vector2(0,-1))
 
+# Get x velocity from LR inputs
 func _inputSequence():
+	
 	if Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
 		playerVelocity.x = playerSpeed
 	elif Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right"):
 		playerVelocity.x = -playerSpeed
 	else:
-		if playerVelocity.x > 1:
-			playerVelocity.x = playerVelocity.x / 5
+		if playerVelocity.x > 1 || playerVelocity.x < -1:
+			playerVelocity.x = playerVelocity.x / 1.1
 		else:
 			playerVelocity.x = 0
