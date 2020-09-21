90e197aedb1097c797fe107bf7b245281851d509
Christian Bortolotti <cbortolo@purdue.edu>
2020-09-21 13:54:05 -0400

    Basic LR
    - Implemented Left-Right movement with an adjustable speed variable
    - Switched to move_and_slide to avoid collision blocking the movement

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 
 # Member variables here
 export var playerGravity = 9.81
+export var playerSpeed = 400
 var playerVelocity = Vector2()
 var playerDistance
 
@@ -15,8 +16,23 @@ func _ready():
 # Called every phys frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	
+	# obtain new y velocity
 	playerVelocity.y += delta * playerGravity 
 	
+	#obtain new x velocity
+	_inputSequence()
+	
 	# distance = velocity * time (right?)
-	playerDistance = playerVelocity * delta
-	move_and_collide(playerDistance)
+	# playerDistance = playerVelocity * delta
+	move_and_slide(playerVelocity, Vector2(0,-1))
+
+func _inputSequence():
+	if Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
+		playerVelocity.x = playerSpeed
+	elif Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right"):
+		playerVelocity.x = -playerSpeed
+	else:
+		if playerVelocity.x > 1:
+			playerVelocity.x = playerVelocity.x / 5
+		else:
+			playerVelocity.x = 0
